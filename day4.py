7820d6396a0c4100a978343e1d9ee3e3ad1fbece
- <->
2021-12-04 12:03:07 +0100

    Day4

---
+++ day4.py
@@ -0,0 +1,79 @@
+from typing import List, Tuple
+
+# Get Data Input
+def read_data(filename : str) -> Tuple[List[int], List[int]]:
+    with open(filename) as f:
+        data = [x.replace("\n", "") for x in f]
+        
+        nums = [int(x) for x in data[0].split(",")]
+        fields = []
+        field = []
+        
+        for idx, line in enumerate(data[2:]):
+            if line == "":
+                fields.append(field)
+                field = []
+            else:
+                field = field + [int(num) for num in line.split(" ") if num != ""]
+                
+        return nums, fields
+
+
+
+def check_field(field : List[int] , nums : List[int])->bool:
+
+    for idx in range(5):
+        # Check Row with idx
+        res = []
+        for elem in [ele for ele in field[5*idx : 5*idx+5]]:
+            res.append(True if elem in nums else False)
+        if all(res): return True
+        
+        # Check Cols with idx
+        res = []
+        for elem in [ele for idx2, ele in enumerate(field) if idx2 % 5 == idx ]:
+            res.append(True if elem in nums else False)
+        if all(res): return True
+            
+    return False
+    
+    
+# Puzzle 1
+def f1(data : List[List[int]]) -> int:
+    nums, fields = data[0], data[1]
+    
+    for idx, lotto in enumerate(nums):
+        if idx < 4: continue
+        
+        for field in fields:
+            if check_field(field, nums[:idx+1]):
+                return sum([ele for ele in field if ele not in nums[:idx+1]]) * lotto
+        
+    
+    
+    
+# Puzzle 2
+def f2(data : List[List[int]]) -> int:
+    nums, fields = data[0], data[1]
+    
+    for idx, lotto in enumerate(nums):
+        if idx < 4: continue
+        
+        for field in fields:
+            if check_field(field, nums[:idx+1]):
+                last_field = field
+                lastNums = nums[:idx+1]
+                fields.remove(field)
+                
+    return sum([ele for ele in last_field if ele not in lastNums]) * lastNums[-1]
+        
+        
+        
+ # MAIN       
+def main(): 
+    data = read_data("Data\\data4.txt")
+    print(f"Puzzle 1: {f1(data)}")
+    print(f"Puzzle 2: {f2(data)}")
+
+if __name__ == "__main__":
+    main()
